3b607cdea23f5c83f6b6870578991e22104353a6
cindycorrea <cindycorrea@byui.edu>
2020-10-08 23:02:58 -0600

    Finally!!

---
+++ lesson4/js/weather.js
@@ -0,0 +1,46 @@
+//Toggle Menu Start
+const compressMenu = document.querySelector('.compress');
+const mainnav = document.querySelector('.navigation');
+
+compressMenu.addEventListener('click', () => {
+    mainnav.classList.toggle('responsive')
+}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {
+    if (window.innerWidth > 1000) mainnav.classList.remove('responsive')
+};
+//End of Toggle Menu
+
+//Current Date Start
+const daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+//const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
+const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+document.getElementById("currentdate").textContent = fulldate;
+//End of Current Date
