2f61222515015abbb0dceb9816c0260b10243651
cindycorrea <cindycorrea@byui.edu>
2020-10-20 09:55:55 -0600

    give me my one point back!

--- lesson5/js/weather.js
+++ lesson5/js/weather.js
@@ -47,7 +47,7 @@ document.getElementById("currentdate").textContent = fulldate;
 
 
 //Pancakes Start
-if (new Date().getDay() == 5) {
+if (d.getDay() == 5) {
     document.getElementById("pancakes").innerHTML = "Saturday = Preston Pancakes in the Park!</br>9:00 a.m. Saturday at the city park pavilion.";
 } else {
     document.getElementById("pancakes").style.display = "none";
