9c313e1185b54bfd8e6306065787d7c798647351
cindycorrea <cindycorrea@byui.edu>
2020-11-11 18:50:30 -0700

    lesson9 addition

---
+++ lesson9/Weather Site/js/lazyLoad.js
@@ -0,0 +1,35 @@
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if (!src) {
+        return;
+    }
+    img.src = src;
+}
+
+const image = document.querySelectorAll("[data-src]");
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px -50px 0px"
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    });
+}, imgOptions);
+image.forEach(image => {
+    imgObserver.observe(image);
+});
+
+const faders = document.querySelectorAll('.fade-in');
+const appearOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px -200px 0px"
+
+};
