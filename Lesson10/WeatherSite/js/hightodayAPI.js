01e53bc85c4d23616960857a38c587e88db33629
cindycorrea <cindycorrea@byui.edu>
2020-11-18 10:04:29 -0700

    fix it!!

--- Lesson10/WeatherSite/js/hightodayAPI.js
+++ Lesson10/WeatherSite/js/hightodayAPI.js
@@ -6,7 +6,8 @@ fetch(apiURL)
         document.getElementById('current').textContent = kelvinToF(jsObject.list[0].main.temp) + "\u00B0F";
         document.getElementById('high').textContent = kelvinToF(jsObject.list[0].main.temp_max) + "\u00B0F";
         document.getElementById('low').textContent = kelvinToF(jsObject.list[0].main.temp_min) + "\u00B0F";
-        document.getElementById('wind').textContent = (6 + jsObject.list[0].wind.speed).toFixed(0) + " mph";
+        document.getElementById('wind').textContent = (jsObject.list[0].wind.speed).toFixed(0) + " mph";
+        document.getElementById('answer').textContent = windChill();
         //const imagesrc = 'http://openweathermap.org/img/wn/' + jsObject.list[0].weather[0].icon + '.png';
         //const desc = jsObject.list[0].weather.description;
         //document.getElementById('imagesrc').textContent = imagesrc;
@@ -19,12 +20,14 @@ function kelvinToF(kelvin) {
     return ((kelvin - 273.15) * 1.8 + 32).toFixed(0);
 }
 
-var temp = parseFloat(document.getElementById("current").innerHTML);
-var wind = parseFloat(document.getElementById("wind").innerHTML);
-if (temp <= 50.0 && wind >= 3.0) {
-    var windchill = 35.74 + (0.6215 * temp) - 35.75 * (Math.pow(wind, 0.16)) + (0.4275 * temp) * (Math.pow(wind, 0.16));
-    var answer = windchill.toFixed(0);
-    document.getElementById("answer").innerHTML = answer + "&#730;F";
-} else {
-    document.getElementById("answer").innerHTML = wind;
+function windChill(temp, wind) {
+    var temp = parseFloat(document.getElementById("current").innerHTML);
+    var wind = parseFloat(document.getElementById("wind").innerHTML);
+    if (temp <= 50 && wind >= 3) {
+        var windchill = 35.74 + (0.6215 * temp) - 35.75 * (wind ** 0.16) + (0.4275 * temp) * (wind ** 0.16);
+        var answer = windchill.toFixed(0);
+        return answer + "\u00B0F";
+    } else {
+        return "N/A";
+    }
 }
